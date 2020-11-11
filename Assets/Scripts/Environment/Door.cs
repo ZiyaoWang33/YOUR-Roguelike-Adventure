c509eb214b856e8fee76c6be45a628901712adb0
alegottu <33403572+alegottu@users.noreply.github.com>
2020-11-10 21:38:42 -0800

    Added door system

---
+++ Assets/Scripts/Environment/Door.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Environment/Room.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics.Tracing;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [HideInInspector] public bool closedWhenEntered = true;
+
+    [SerializeField] private Door[] doors = null;
+    [SerializeField] private Enemy[] enemies = null;
+
+    private bool active = false;
+    private int deathCount = 0;
+    private bool completed = false;
+
+    private const string playerTag = "Player";
+
+    private void OnEnable()
+    {
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.OnDeath += OnEnemyDeathEventHandler;
+        }
+    }
+
+    private void OnEnemyDeathEventHandler()
+    {
+        if (active)
+        {
+            deathCount++;
+        }
+
+        if (deathCount >= enemies.Length)
+        {
+            foreach(Door door in doors)
+            {
+                door.Open();
+                completed = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == playerTag)
+        {
+            CameraController.instance.ChangeTarget(transform);
+            active = true;
+
+            if (!completed)
+            {
+                foreach (Door door in doors)
+                {
+                    door.Close();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == playerTag)
+        {
+            active = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.OnDeath -= OnEnemyDeathEventHandler;
+        }
+    }
+}
