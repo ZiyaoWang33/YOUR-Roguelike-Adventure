d340884cd0390bdee43b8a188ac92d4d75edf9d8
alegottu <33403572+alegottu@users.noreply.github.com>
2020-11-12 03:04:17 -0800

    Added extra door fixes

---
+++ Assets/Scripts/Map/Room.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [HideInInspector] public bool closedWhenEntered = true;
+
+    [SerializeField] private RoomActivator activator = null;
+
+    private const string playerTag = "Player";
+
+    private void Awake()
+    {
+        if (closedWhenEntered)
+        {
+            activator.closedWhenEntered = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == playerTag)
+        {
+            CameraController.instance.ChangeTarget(transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Map/RoomActivator.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public class RoomActivator : MonoBehaviour
+{
+    [HideInInspector] public bool closedWhenEntered = false;
+
+    [SerializeField] private Door[] doors = null;
+    [SerializeField] private Enemy[] enemies = null;
+
+    private bool active = false;
+    private int deathCount = 0;
+    private bool completed = false;
+
+    private const string playerTag = "Player";
+
+    private void OnEnable()
+    {
+        if (closedWhenEntered)
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.OnDeath += OnEnemyDeathEventHandler;
+            }
+        }
+    }
+
+    private void OnEnemyDeathEventHandler()
+    {
+        if (active)
+        {
+            deathCount++;
+        }
+
+        if (deathCount >= enemies.Length)
+        {
+            foreach(Door door in doors)
+            {
+                door.Open();
+                completed = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == playerTag)
+        {
+            active = true;
+
+            if (!completed)
+            {
+                foreach (Door door in doors)
+                {
+                    door.Close();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == playerTag)
+        {
+            active = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (closedWhenEntered)
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.OnDeath -= OnEnemyDeathEventHandler;
+            }
+        }
+    }
+}
