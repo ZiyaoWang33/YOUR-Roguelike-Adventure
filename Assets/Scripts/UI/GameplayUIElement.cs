bda8fd7c0330785506e94737c17962fb9047aa75
alegottu <33403572+alegottu@users.noreply.github.com>
2021-01-21 23:09:11 -0800

    Fixed UI Component Issues

---
+++ Assets/Scripts/UI/GameplayUIElement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public abstract class GameplayUIElement : MonoBehaviour
+{
+    [SerializeField] protected GameObject element = null;
+
+    protected Player player = null;
+
+    protected virtual void Awake()
+    {
+        Player.OnPlayerEnter += OnPlayerEnterEventHandler;
+    }
+
+    protected virtual void OnPlayerEnterEventHandler(Player player)
+    {
+        element.SetActive(true);
+
+        this.player = player;
+        player.OnPlayerExit += OnPlayerExitEventHandler;
+    }
+
+    protected virtual void OnPlayerExitEventHandler()
+    {
+        element.SetActive(false);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        Player.OnPlayerEnter -= OnPlayerEnterEventHandler;
+        player.OnPlayerExit -= OnPlayerExitEventHandler;
+    }
+}
