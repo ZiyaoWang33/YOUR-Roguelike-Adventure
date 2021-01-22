087a702e945d5ddf0d47656acf8dfbefec300f77
vinhandyle <vinhandyle@gmail.com>
2021-01-21 22:50:43 -0800

    GameplayUIElement placeholder

---
+++ Assets/Scripts/UI/GameplayUIElement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayUIElement : MonoBehaviour
+{
+    protected virtual void OnPlayerEnterEventHandler(Player player)
+    {
+       
+    }
+
+    protected virtual void OnDestroy()
+    {
+
+    }
+}
