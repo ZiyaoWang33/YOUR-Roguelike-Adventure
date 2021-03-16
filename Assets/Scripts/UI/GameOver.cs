f4f0461273499dc515d2571a66f26cc80cb39bc7
alegottu <33403572+alegottu@users.noreply.github.com>
2021-03-16 04:58:09 -0700

    Game over screen added

---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameOver : GameplayUIElement
+{
+    protected override void OnPlayerEnterEventHandler(Player player)
+    {
+        this.player = player;
+        player.OnPlayerExit += OnPlayerExitEventHandler;
+    }
+
+    protected override void OnPlayerExitEventHandler()
+    {
+        element.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UI/QuitToMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class QuitToMenu : SceneTransition, IMenuButton
+{
+    public void OnClick()
+    {
+        transform.parent.gameObject.SetActive(false);
+        SceneController.Instance.UnloadLevel("MapPhase");
+        SceneController.Instance.LoadLevel("Menu");
+    }
+}
